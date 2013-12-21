2273eff012845574f7017af7400858bfc35cf2ae
tchipwanya <tchipwanya@GOSA.(none)>
2013-12-21 11:25:08 -0500

    Initial commit

---
+++ generate/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'f1047ff38bbd684fa5e92613cd6fe20c7e4c490b9fd751bc953ea49e42d38ba9ffbbae255855b424363c75a2e7bdc53deeaf67f533ff7b8d410f918a593f2954';
---
+++ generate/config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_generate_session',
+  :secret      => '1199538935adc60a1dd1255895020f17f8d6e061d2cf0ad086d0de30c2685bd79c48615e171ac1a135997caf46c9ace18995a0a6960607b43ddf1d78c1bee8d9'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ActionController::Base.session_store = :active_record_store
